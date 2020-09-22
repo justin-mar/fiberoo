c2d5d12e558ab38292c1390f8129773926e614c3
justin-mar <70727597+justin-mar@users.noreply.github.com>
2020-09-22 09:38:37 -1000

    Update

---
+++ fiberoo.js
@@ -0,0 +1,13 @@
+function fib() {
+  const num = [0, 1];
+  for (let i = 2; i < 50; i++) {
+    num[i] = num[i - 1] + num[i - 2];
+  }
+  return num;
+}
+
+console.log(fib());
+
+function numToStrings(arr) {
+  _.map(arr, function(num){ return })
+}
